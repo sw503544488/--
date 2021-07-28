f59832c1470a873be377e0203aadb7caa2b64bf7
sw503544488 <503544488@qq.com>
2021-07-28 09:49:55 +0800

    fixed bug

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const $siteList = $('.siteList')
-const $lastLi = $siteList.find('li.last')
+const $lastLi = $siteList.find('li.last ')
 const x = localStorage.getItem('x')
 const xObject = JSON.parse(x) //字符串变对象
 const removehttp = (url) => {
@@ -38,15 +38,14 @@ const hashMap = xObject || [{
 
 
 const render = () => {
-  a = $siteList.find('li:not(.last)').remove()
-  hashMap.forEach((node, index) => {
+  Array.isArray(hashMap) && hashMap.forEach((node, index) => {
 
     const $li = $(`<li>
     
         <div class="site">
           <div class="logo">${removehttp(node.url)[0].toUpperCase()}</div>
         
-         <span class="imgcontainer"><img src=${node.logo} onerror="this.src='errorimg.jpg' " class='imgicon' width='25px' height='25px' alt="1" ></span>
+         <span class="imgcontainer"><img src=${node.logo}  onerror="this.src='errorimg.jpg'" class='imgicon' width='25px' height='25px' alt="1" ></span>
           <div class="link">${ removehttp(node.url) }</div>
        <div class="close"><img src='close.png'  width='10px' height='10px' class='closeimg' ></div>
          </div>
@@ -58,7 +57,7 @@ const render = () => {
     })
 
     $li.on('click', ".close", (e) => {
-      e.stopPropagation()
+      // e.stopPropagation()
       hashMap.splice(index, 1)
       render()
     })
@@ -86,7 +85,22 @@ window.onbeforeunload = () => {
   const string = JSON.stringify(hashMap) //将hashMap变成字符串
   localStorage.setItem('x', string)
 }
+let focus = 0
+$('#inputbox').on('focus', (e) => {
+  focus = 1
+  console.log(focus)
+})
+$('#inputbox').on('blur', (e) => {
+  focus = 0
+  console.log(focus)
+})
+
+
 $(document).on('keypress', (e) => { // 点击事件
+  if (focus === 1) {
+    return
+
+  }
   console.log(e.key)
   let n = []
 
@@ -111,29 +125,17 @@ $(document).on('keypress', (e) => { // 点击事件
 
 
 $(document).on('keypress', (e) => {
+  if (focus === 1) {
+    return
 
+  }
   const {
     key
   } = e
-  let a = {
-    1: 1,
-    2: 2,
-    3: 3,
-    4: 2,
-    5: 2,
-    6: 2,
-    7: 2,
-    8: 2,
-    9: 2,
-    0: 2
-  }
-  if (key in a) {
-    let c = key - 1;
-    if (c === -1) {
-      window.open(hashMap[10].url)
 
-    }
-    window.open(hashMap[c].url)
+  if (key && hashMap[key - 1]) {
+
+    window.open(hashMap[key - 1].url)
   }
 
 })
